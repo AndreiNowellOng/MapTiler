3f106fb92653245ef6c589f4195c9bfd122a5fab
AndreiNowellOng <162392272+AndreiNowellOng@users.noreply.github.com>
2024-03-11 12:26:02 +0800

    Add user location

--- App.tsx
+++ App.tsx
@@ -1,43 +1,80 @@
-import React, {Component} from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, TouchableOpacity, View} from 'react-native';
-import MapLibreGL, {
-  Light,
-  MapView,
-  MarkerView,
-} from '@maplibre/maplibre-react-native';
+import GetLocation from 'react-native-get-location';
+import MapLibreGL, {Logger} from '@maplibre/maplibre-react-native';
+import Geolocation from '@react-native-community/geolocation';
 
 // Will be null for most users (only Mapbox authenticates this way).
 // Required on Android. See Android installation notes.
+
 MapLibreGL.setAccessToken(null);
 const MAPTILER_API_KEY = 'f7o9viz24aowHK4B26RH';
 
+// edit logging messages
+Logger.setLogCallback(log => {
+  const {message} = log;
+  if (
+    message.match('Request failed due to a permanent error: Canceled') ||
+    message.match('Request failed due to a permanent error: Socket Closed')
+  ) {
+    return true;
+  }
+  return false;
+});
 
-export default function App() {
+function App() {
+  const [mapLocation, setMapLocation] = useState({
+    lat: 0,
+    long: 0,
+  });
+
+  GetLocation.getCurrentPosition({
+    enableHighAccuracy: true,
+    timeout: 60000,
+  })
+    .then(location => {
+      setMapLocation({
+        ...mapLocation,
+        lat: location.latitude,
+        long: location.longitude,
+      });
+      // console.log(location.latitude);
+      // latitude = location.latitude;
+      // longitude = location.longitude
+    })
+    .catch(error => {
+      const {code, message} = error;
+      console.warn(code, message);
+    });
+
   return (
     <View style={styles.page}>
       <MapLibreGL.MapView
         style={styles.map}
         logoEnabled={false}
-        styleURL={`https://api.maptiler.com/maps/streets-v2/style.json?key=${MAPTILER_API_KEY}`}
-        styleJSON={`https://api.maptiler.com/maps/streets-v2/256/tiles.json?key=${MAPTILER_API_KEY}`}>
-          
+        styleURL={`https://api.maptiler.com/maps/bright/style.json?key=${MAPTILER_API_KEY}`}
+        onPress={feature =>
+          console.log('Coords:', feature.geometry.coordinates)
+        }>
         <MapLibreGL.Camera
           zoomLevel={15}
-          centerCoordinate={[121.06827, 14.66433]}
+          centerCoordinate={[mapLocation.long, mapLocation.lat]}
           pitch={10}
         />
-        <MapLibreGL.PointAnnotation 
-          coordinate={[121.06827, 14.66433]}
+        <MapLibreGL.UserLocation visible={true} />
+        {/* <MapLibreGL.PointAnnotation
+          coordinate={[longitude, latitude]}
           id="mark"
-          title='this is a point annotation'
-          draggable={true}
-          ></MapLibreGL.PointAnnotation>
+          title="this is a point annotation"
+          draggable={true}>
+          <Icon size={14} reverse type="ionicon" name="home-outline" />
+        </MapLibreGL.PointAnnotation> */}
       </MapLibreGL.MapView>
     </View>
   );
 }
 
-
+export default App;
 const styles = StyleSheet.create({
   page: {
     flex: 1,
@@ -62,4 +99,4 @@ const styles = StyleSheet.create({
     color: 'white',
     fontWeight: 'bold',
   },
-});X
+});
